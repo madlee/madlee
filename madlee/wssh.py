c582149c638a4c40a4b4a73ee0f8115fe3adf72e
Madlee <madlee@sohu.com>
2022-12-20 16:08:32 +0800

    add sync client

--- madlee/wssh.py
+++ madlee/wssh.py
@@ -229,7 +229,7 @@ async def server(host, port):
         await aio.Future()  # run forever
 
 
-class Client:
+class AsyncClient:
     def __init__(self, url):
         self.url = url
         self.client = None
@@ -433,6 +433,200 @@ class Client:
 
 
 
+class SyncClient:
+    def __init__(self, url):
+        from websocket import WebSocket
+        self.url = url
+        self.client = WebSocket()
+
+    def connect(self, username, secret_key):
+        self.client.connect(self.url)
+        if username:
+            msg = {
+                'id': str(uuid4()), 'cmd': 'login',
+                'username': username,
+                'timestamp': DateTime.now().isoformat()
+            }
+
+            signature = ':'.join([msg['id'], msg['cmd'], username, msg['timestamp'], secret_key])
+            msg['signature'] = 'MD5:' + MD5(signature.encode()).hexdigest()
+            self.client.send(dump_json(msg))
+            response = load_json(self.client.recv())
+            if response['status'] != 'OK':
+                raise RuntimeError(response.get('message', 'Login Failed'))
+        return self.client
+
+
+    def close(self):
+        self.client.close()
+
+
+    def run_shell(self, cmd):
+        id = str(uuid4())
+        self.client.send(dump_json({
+            'id': id,
+            'cmd': cmd
+        }))
+        msg = self.client.recv()
+        return load_json(msg), id
+
+    def send_file(self, target, filename, xz=True):
+        file = open(filename, 'rb')
+        id = str(uuid4())
+        if xz:
+            encode = 'xz'
+        else:
+            encode = 'raw'
+
+        self.client.send(dump_json({
+            'id': id,
+            'cmd': 'open',
+            'encode': encode,
+            'target': target
+        }))
+
+        raw_size = length = 0
+
+        md5 = MD5()
+        while True:
+            content = file.read(BUFFER_SIZE)
+            if content:
+                md5.update(content)
+                raw_size += len(content)
+                if xz:
+                    content = compress(content)
+                    encode = 'xz'
+                else:
+                    encode = 'raw'
+                length += len(content)
+                self.client.send(
+                    b'\0'+ content
+                )
+            else:
+                break
+        
+        md5 = md5.hexdigest()
+        self.client.send(dump_json({
+            'id': id, 'cmd': 'close',
+            'raw_size': raw_size, 'length': length
+        }))
+
+        for msg in self.client:
+            msg = load_json(msg)
+            if 'cmd' in msg and msg['cmd'] == 'close':
+                if md5 == msg['md5']:
+                    break
+                else:
+                    raise RuntimeError('Transfer File %s --> %s failed.' % (filename, target))
+        return msg, id
+        
+
+    def redis(self, url):
+        id = str(uuid4())
+        msg = {
+            'id': id, 'cmd': 'redis',
+            'call': 'open',
+            'url': url
+        }
+        self.client.send(dump_json(msg))
+        msg = self.client.recv()
+        self.has_redis = True
+        return load_json(msg), id
+
+    def rpush(self, key, *data):
+        assert self.has_redis
+        assert len(data)>0
+        if type(data[0]) == bytes:
+            data = [
+                b64encode(row).decode()
+                for row in data
+            ]
+            encode='base64'
+        else:
+            encode='raw'
+
+        id = str(uuid4())
+        msg = {
+            'id': id, 'cmd': 'redis',
+            'call': 'rpush', 'key': key,
+            'encode': encode,
+            'data': data
+        }
+        self.client.send(dump_json(msg))
+        msg = self.client.recv()
+        return load_json(msg)
+
+
+    def publish(self, key, data):
+        assert self.has_redis
+        if type(data) == bytes:
+            data = b64encode(data)
+            encode='base64'
+        else:
+            encode='raw'
+        
+        id = str(uuid4())
+        msg = {
+            'id': id, 'cmd': 'redis',
+            'call': 'publish', 'key': key,
+            'encode': encode,
+            'data': data
+        }
+        self.client.send(dump_json(msg))
+        msg = self.client.recv()
+        return load_json(msg), id
+
+    def pull_file(self, filename, target, xz=True):
+        id = str(uuid4())
+        msg = {
+            'id': id, 'cmd': 'pull',
+            'encode': 'xz' if xz else 'raw',
+            'filename': filename
+        }
+        print (msg)
+        self.client.send(dump_json(msg))
+
+        with open(target, 'wb') as output:
+            md5 = MD5()
+            for msg in self.client:
+                if type(msg) == bytes:
+                    content = msg[1:]
+                    if xz:
+                        content = decompress(content)
+                    md5.update(content)
+                    output.write(content)
+                else:
+                    break
+            msg = load_json(msg)
+            print ('####', md5.hexdigest())
+            return msg, id
+
+
+    @staticmethod
+    def batch(url, username, secret, cmds):
+        client = SyncClient(url)
+        client.connect(username, secret)
+        i = 0
+        while i < len(cmds):
+            if cmds[i] == 'pull':
+                src = cmds[i+1]
+                tgt = cmds[i+2]
+                result = client.pull_file(src, tgt, False)
+                i += 3
+            elif cmds[i] == 'send':
+                src = cmds[i+1]
+                tgt = cmds[i+2]
+                result = client.send_file(tgt, src, False)
+                i += 3
+            else:
+                result = client.run_shell(cmds[i])
+                i += 1
+            print (result)
+
+        client.close()
+
+
+
 
 if __name__ == '__main__':
     import sys
@@ -442,3 +636,5 @@ if __name__ == '__main__':
     SECRET_KEY = config['USERS']
     HOST_NAME = hostname()
     aio.run(server(host, int(port)))
+
+
