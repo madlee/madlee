24fcbeb9cc691253272e1cbbc4fc66f82a46b435
Madlee <madlee@sohu.com>
2023-01-11 20:55:15 +0800

    add lazy_property

--- madlee/misc/__init__.py
+++ madlee/misc/__init__.py
@@ -5,3 +5,15 @@ from .time import *
 from .file import *
 from .io   import *
 
+
+def lazy_property(func):
+    name = '_lazy_' + func.__name__
+    @property
+    def lazy(self):
+        if hasattr(self, name):
+            return getattr(self, name)
+        else:
+            value = func(self)
+            setattr(self, name, value)
+            return value
+    return lazy
